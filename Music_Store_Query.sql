879c35c68d17bb9d5578294f6ac7d92b76ef332b
SURAJ KUMAR <128514063+suraj15122000@users.noreply.github.com>
2024-07-31 14:44:34 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -5,7 +5,7 @@
 SELECT title, last_name, first_name 
 FROM employee
 ORDER BY levels DESC
-LIMIT 1
+LIMIT 1;
 
 
 /* Q2: Which countries have the most Invoices? */
@@ -13,26 +13,21 @@ LIMIT 1
 SELECT COUNT(*) AS c, billing_country 
 FROM invoice
 GROUP BY billing_country
-ORDER BY c DESC
+ORDER BY c DESC LIMIT 1;
 
 
 /* Q3: What are top 3 values of total invoice? */
 
 SELECT total 
 FROM invoice
-ORDER BY total DESC
+ORDER BY total DESC LIMIT 3;
 
 
 /* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
 Write a query that returns one city that has the highest sum of invoice totals. 
 Return both the city name & sum of all invoice totals */
 
-SELECT billing_city,SUM(total) AS InvoiceTotal
-FROM invoice
-GROUP BY billing_city
-ORDER BY InvoiceTotal DESC
-LIMIT 1;
-
+SELECT billing_city, SUM(total)AS S FROM INVOICE GROUP BY billing_city ORDER BY S DESC LIMIT 1;
 
 /* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
 Write a query that returns the person who has spent the most money.*/
